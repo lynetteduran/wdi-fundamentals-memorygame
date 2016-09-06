86269d164da14ebb7a89081ca79a4e6b2552cd6a
lynetteduran <lyndduran@gmail.com>
2016-09-05 20:43:26 -0700

    added if statements comparing all combinations between cardOne thru cardFour resulting in alerts for both true and false conditions.

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,43 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+
+if (cardOne === cardTwo) {
+	alert("You've made a match!");
+}	else {
+	alert("Sorry, try again.");	
+}
+
+if (cardOne === cardThree) {
+	alert("You've made a match!");
+}	else {
+	alert("Sorry, try again.");	
+}
+
+if (cardOne === cardFour) {
+	alert("You've made a match!");
+}	else {
+	alert("Sorry, try again.");	
+}
+
+if (cardTwo === cardThree) {
+	alert("You've made a match!");
+}	else {
+	alert("Sorry, try again.");	
+}
+
+if (cardTwo === cardFour) {
+	alert("You've made a match!");
+}	else {
+	alert("Sorry, try again.");	
+}
+
+if (cardThree === cardFour) {
+	alert("You've made a match!");
+}	else {
+	alert("Sorry, try again.");	
+}
+
+
